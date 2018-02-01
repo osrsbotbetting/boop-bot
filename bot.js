9d8fb78406602e5b39ef41b3604679557a0032b1
1510749 <31429765+1510749@users.noreply.github.com>
2018-02-01 01:39:57 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,70 @@
+const settings = require("./settings.json");
+const prefix = settings.prefix;
+const Discord = require('discord.js');
+const fs = require("fs");
+
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+bot.mutes = require("./mutes.json");
+bot.levelD = require("./levelD.json");
+
+
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.error(err);
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands to load.");
+    }
+    console.log(`Loading ${jsfiles.length} commands.`);
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${i + 1}: ${f} loaded.`);
+        bot.commands.set(props.help.name, props);
+    });
+});
+
+bot.on("ready", () => {
+    console.log(`Bot is ready! ${bot.user.username}`);
+    bot.user.setGame("Beep Brown!")
+
+    bot.setInterval(() => {
+        for(let i in bot.mutes) {
+            let time = bot.mutes[i].time;
+            let guildId = bot.mutes[i].guild;
+            let guild = bot.guilds.get(guildId);
+            let member = guild.members.get(i);
+            let mutedRole = guild.roles.find(r => r.name === "Muted");
+            if(!mutedRole) continue;
+
+            if(Date.now() > time && time != null) {
+                console.log(`${i} is now able to be unmuted!`);
+                member.removeRole(mutedRole);
+                delete bot.mutes[i];
+                fs.writeFile("./mutes.json", JSON.stringify(bot.mutes), err => {
+                    if(err) throw err;
+                    console.log(`${member.user.tag} has been unmuted.`);
+                });
+            }
+        }
+
+    }, 1000);
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type == "dm") return;
+
+    let messageArray = message.content.split(/\s+/g);
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot, message, args);
+    
+});
+
+bot.login(settings.token);
