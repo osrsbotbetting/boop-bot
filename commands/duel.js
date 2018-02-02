9c2c3cf1dc63ff2f6a0a25fb91841147b48a04c6
1510749 <31429765+1510749@users.noreply.github.com>
2018-02-01 20:47:12 -0800

    Update duel.js

--- commands/duel.js
+++ commands/duel.js
@@ -28,10 +28,7 @@ module.exports.run = async (bot, message, args) => {
         "monitor", "spanish dildo", "volleyball", "KNOWLEDGE", "Coke", "Pepsi", "basketball",
         "pencil", "pen", "staple", "ceramic pot", "paperclip", "iPhone 4S", "shuriken"];
 
-    var specificActions = [`${toDuel}(${defenderHP}hp) took ${duelEE}(${attackerHP}hp) to McDonalds`,
-        `${toDuel}(${defenderHP}hp) installed Vosteran on ${duelEE}(${attackerHP}hp)'s computer`,
-        `${toDuel}(${defenderHP}hp) downvoted ${duelEE}(${attackerHP}hp)'s Reddit post`,
-        `${toDuel}(${defenderHP}hp) convinced ${duelEE}(${attackerHP}hp) to not commit suicide`];
+
 
     var duelEE = message.author;
     var toDuel = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
@@ -147,6 +144,10 @@ module.exports.run = async (bot, message, args) => {
         }
     }
     function play(toDuel1, duelEE1, attackerHP, defenderHP, color){
+            var specificActions = [`${toDuel1}(${defenderHP}hp) took ${duelEE1}(${attackerHP}hp) to McDonalds`,
+            `${toDuel1}(${defenderHP}hp) installed Vosteran on ${duelEE1}(${attackerHP}hp)'s computer`,
+            `${toDuel1}(${defenderHP}hp) downvoted ${duelEE1}(${attackerHP}hp)'s Reddit post`,
+            `${toDuel1}(${defenderHP}hp) convinced ${duelEE1}(${attackerHP}hp) to not commit suicide`];
             let scenario = randomInt(0,3);
             console.log(scenario);
             if(scenario == 0){
