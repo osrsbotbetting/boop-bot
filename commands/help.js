57579d5d81148fefb6823e2fd056dadb0bedcec0
1510749 <31429765+1510749@users.noreply.github.com>
2018-02-07 19:48:36 -0800

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js'); 
+const fs = require('fs');
+const prefix = "!";
+module.exports.run = async (bot, message, args) => {
+    var clist = [];
+    var cinfo = [" @ : Displays user's avatar",
+    " (@)/(profile) : Duels another user/ view dueling profiles",
+    " : Displays Bot commands",
+    " @/# : Mutes a user",
+    "(cc/ud/mc)/#/?/ans : Create a poll",
+    "#~# : Generates a random number in range",
+    " @ : Unmutes a user", " @/Id : Displays user's info"
+    ];
+    fs.readdir("./commands/", (err, files) => {
+        if(err) console.error(err);
+        let cfiles = files.filter(f => f.split(".").pop() === "js");
+        cfiles.forEach((f, i) => {
+            clist.push(`\`${prefix + f.split(".").splice(0, 1)}\``);
+        });
+        for(i in clist){
+            clist[i] += cinfo[i];
+        }
+        console.log(clist.join('\n'))
+        let embed = new Discord.RichEmbed()
+            .setColor("#61eeaa")
+            .setAuthor("Bot Commands")
+            .setDescription(clist);
+            
+        message.channel.send({embed: embed});
+    });
+
+}
+
+module.exports.help = {
+    name: "help"
+}
