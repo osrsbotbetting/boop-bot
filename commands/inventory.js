e668249a78d61a0d1038618ed0740830cc09dbc0
1510749 <31429765+1510749@users.noreply.github.com>
2018-02-10 19:12:01 -0800

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -1,32 +1,47 @@
 const Discord = require('discord.js');
 const fs = require("fs");
+
 module.exports.run = async (bot, message, args) => {
     let inventoryOwner = message.author;
-    console.log("here");
     if(!bot.inventory[inventoryOwner.id]){
-        console.log("here");
         bot.inventory[inventoryOwner.id] = {
             guild : message.guild.id,
-            Browncoins : 0
+            Browncoins : 0,
         }
-        console.log("here");
         fs.writeFile("./inventory.json", JSON.stringify(bot.inventory), err => {
             if(err) throw err;
             message.channel.send("An inventory has been created for you.");
             console.log(`${inventoryOwner.tag}'s inventory has been created.`);
         });
     }
-    let embed = new Discord.RichEmbed()
-        .setAuthor(`${inventoryOwner.tag}'s Inventory`)
-        .setColor("#005EFF")
-        console.log(bot.inventory[inventoryOwner.id]);
-        for(i in bot.inventory[inventoryOwner.id]){
-            console.log(i)
-            if(i == "Browncoins"){
-                embed.addField(`__${i}__ :`, bot.inventory[inventoryOwner.id].Browncoins, true);
+    if(!args[0]){
+        let embed = new Discord.RichEmbed()
+            .setAuthor(`${inventoryOwner.tag}'s Inventory`)
+            .setColor("#005EFF")
+            console.log(bot.inventory[inventoryOwner.id]);
+            for(i in bot.inventory[inventoryOwner.id]){
+                console.log(i)
+                if(i != "guild"){
+                    embed.addField(`__${i}__ :`, bot.inventory[inventoryOwner.id][i], true);
+                }
             }
-        }
-    message.channel.send({embed:embed})
+        return message.channel.send({embed:embed});
+    }
+    else{
+        for(i in bot.inventory[inventoryOwner.id]){
+            if(i == args[0]){
+                let embed = new Discord.RichEmbed()
+                    .setAuthor(`${inventoryOwner.tag}'s Inventory has`)
+                    .setColor("#005EFF")
+                    console.log(bot.inventory[inventoryOwner.id]);
+                    if(i != "guild"){
+                        embed.addField(`__${i}__ :`, bot.inventory[inventoryOwner.id][i], true);
+                        return message.channel.send({embed:embed});
+                    }
+            }
+        }      
+        return message.channel.send("```You do not have any"+ ` ${args[0]}.`+"```")  
+    }
 }
 
 module.exports.help = {
